b97b77754bf696c7dc5f6d5b48028f397e487ea3
MaguKill <94996470+MaguKill@users.noreply.github.com>
2024-10-22 22:36:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-      user: process.env.EMAIL_USER,  
-      pass: process.env.EMAIL_PASS 
+      user: 'contactoregistro2@gmail.com',  
+      pass: 'fgpnsxaoamuffixq' 
     }
   });
   
