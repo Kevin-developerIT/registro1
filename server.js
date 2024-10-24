0d596f794a9cd15a0c6c9eac2bf1a3ee0fd12449
MaguKill <94996470+MaguKill@users.noreply.github.com>
2024-10-24 12:21:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/register', (req, res) => {
         const mailOptions = {
             from: process.env.EMAIL_USER,
             to: correo,
-            subject: 'Confirmación de registro -Cena de Celebración Fundación Alsea',
+            subject: 'Confirmación de registro - Cena de celebración Fundación Alsea',
             html: `
         <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 5px; background-color: #f9f9f9;">
             <h2 style="text-align: center; color: #333;">¡Gracias por registrarte, ${nombre}!</h2>
@@ -68,7 +68,7 @@ app.post('/register', (req, res) => {
 
             <!-- Imagen del evento -->
             <a href="https://maps.app.goo.gl/1FcABA5NdDyUn1wh8" target="_blank"
-            <img src="https://hgroup.consulting/wp-content/uploads/2024/10/invitacion.jpg" alt="Imagen del evento" style="width: 100%; height: auto; border-radius: 5px;">
+            <img src="https://hgroup.consulting/wp-content/uploads/2024/10/Invitaciones.png" alt="Imagen del evento" style="width: 100%; height: auto; border-radius: 5px;">
             </a>
             <!-- Localización del evento -->
             <h3 style="text-align: center; color: #333;">Ubicación del encuentro</h3>
